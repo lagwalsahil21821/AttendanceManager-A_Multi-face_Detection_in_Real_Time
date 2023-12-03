ecbe0d9050ef8653d33d7d3e8bb13b55985a1472
lagwalsahil21821 <lagwalsahil7978@gmail.com>
2023-12-03 18:48:13 +0530

    Account UI

--- app/src/main/java/com/example/attendancemanager_multi_facedetectioninrealtime/MainActivity.kt
+++ app/src/main/java/com/example/attendancemanager_multi_facedetectioninrealtime/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.attendancemanager_multi_facedetectioninrealtime
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adminCard = findViewById<LinearLayout>(R.id.adminCard)
+        val teacherCard = findViewById<LinearLayout>(R.id.teacherCard)
+        val studentCard = findViewById<LinearLayout>(R.id.studentCard)
+
+        adminCard.setOnClickListener {
+            intent = Intent(this@MainActivity, AdminLoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        teacherCard.setOnClickListener {
+            intent = Intent(this@MainActivity, TeacherLoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        studentCard.setOnClickListener {
+            val intent = Intent(this@MainActivity, StudentLoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
